62fef2f5fd14927a45b76559efa0363ac5750309
Kyle Powers <kylepowers@gmail.com>
2014-11-10 21:45:53 -0800

    Update tinder.js

--- tinder.js
+++ tinder.js
@@ -1,5 +1,5 @@
 var TINDER_HOST = "https://api.gotinder.com";
-var request = require('request');
+var request = require('browser-request');
 
 /**
  * Constructs a new instance of the TinderClient class
